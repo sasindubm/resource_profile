413b8d9fe9e7461f28c8871737c9c6c47abb1c26
januli11 <januli880@gmial.com>
2025-10-15 13:31:08 +0530

    10/15 januli

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::get('/waterform', function () {
+    return view('waterform');
+});
 require __DIR__.'/auth.php';
