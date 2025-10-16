05b83469095b9fc3d914bdd3bcf69d3f185cf205
sasindubm <msasindu450@gmail.com>
2025-10-16 15:41:16 +0530

    10/16 sasindu

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'occupation',
         'password',
     ];
 
@@ -44,5 +45,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function gnd()
+    {
+        return $this->belongsTo(GramaNiladariDivision::class, 'gnd_code', 'gnd_code');
     }
 }
